eb94c462c7a208e732057d144b1a8a504ea2b224
jwilliams <jonathan.williams2205@gmail.com>
2020-04-17 11:00:29 -0400

    v0.1.1 UI

---
+++ app/src/main/java/com/hciclassproject/arhomeimprovement/scan_furniture.java
@@ -0,0 +1,131 @@
+package com.hciclassproject.arhomeimprovement;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link scan_furniture#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class scan_furniture extends Fragment{
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public scan_furniture() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment scan_furniture.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static scan_furniture newInstance(String param1, String param2) {
+        scan_furniture fragment = new scan_furniture();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scan_furniture, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Toast infoMessage = Toast.makeText(getActivity().getApplicationContext(), "You can take a picture of your furniture and save it for later!", Toast.LENGTH_LONG);
+        infoMessage.setGravity(Gravity.TOP, 0, 0);
+        infoMessage.show();
+
+        view.findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(scan_furniture.this)
+                        .navigate(R.id.action_scan_furniture_to_home_page);
+            }
+        });
+
+        view.findViewById(R.id.scan_button).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                final PopupMenu popup = new PopupMenu(getActivity(), getView().findViewById(R.id.scan_button));
+                popup.getMenuInflater().inflate(R.menu.popup_menu_scan_furniture, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.save_scan:
+                                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                builder.setTitle("Save Furniture As?");
+                                final EditText input = new EditText(getActivity());
+                                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                                builder.setView(input);
+
+                                builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        mParam1 = input.getText().toString();
+                                    }
+                                });
+
+                                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                                builder.show();
+                                return false;
+                            case R.id.cancel_scan:
+                                popup.dismiss();
+                        }
+                        return false;
+                    }
+                });
+                popup.show();
+            }
+        });
+    }
+}
